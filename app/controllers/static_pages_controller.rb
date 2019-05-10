bc7983aa19032613ef87b86f42c5601718d8aaef
thread314 <eddie.drury@gmail.com>
2019-05-10 19:50:03 +0800

    added dynamic title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
 
+	def setup
+		@var = "borgon"
+	end
+	
   def home
+		#render html: "this is the word of the day: #{@var}"
   end
 
   def help
