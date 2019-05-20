194989bd8b63c4aceedaf4b983e4fe69816996be
thread314 <eddie.drury@gmail.com>
2019-05-20 19:15:46 +0800

    added study view and helper function to parse question

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,10 @@ class UsersController < ApplicationController
 		helpers.addnewcards(@user)
   end
 
+	def study
+		@user = User.find(params[:id])
+		@nextcard = helpers.getnextcard(@user)
+		@questionarray = helpers.separatequestion(@nextcard.card.levels.first.question)
+	end
+
 end
