d9cd6c90d6a4a7e680841b6a2d29c88dbf615799
thread314 <eddie.drury@gmail.com>
2019-05-24 13:13:32 +0800

    -

--- app/controllers/cardstates_controller.rb
+++ app/controllers/cardstates_controller.rb
@@ -1,6 +1,7 @@
 class CardstatesController < ApplicationController
 
   def edit
+		#Comment
 		@user = User.find(params[:id])
 		@nextcard = helpers.getnextcard(@user)
 		@questionarray = helpers.separatequestion(@nextcard.card.levels.first.question)
