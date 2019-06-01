9efd31ad47600667978b15d26cf17df6db05813d
thread314 <eddie.drury@gmail.com>
2019-06-01 13:56:28 +0800

    added create and edit users:w

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
   
 	def new
+		@newuser = User.new
   end
 	
+	def create
+		@newuser = User.create(user_params)
+		redirect_to @newuser
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to @user
+		end
+	end
+
 	def index
 		@users = User.all
 	end
@@ -24,4 +41,9 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
   end
 
+	private
+
+	def user_params
+		params.require(:user).permit(:name)
+	end
 end
